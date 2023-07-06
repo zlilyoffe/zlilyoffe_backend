7c03f52fcc9eaf1464dc978714b4fd44b8f8d335
Zlil <zlilyoffe@gmail.com>
2023-07-06 23:18:34 +0300

    reduced api to blog count

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongoose.connect(dbURI,{useUnifiedTopology:true}).then(
     (result) => {
         console.log('connected to db');
         app.listen(4000);
-        console.log('start listenning on port 3000');
+        console.log('start listenning on port 4000');
     })
     .catch((err)=> console.log(err));
 
@@ -22,66 +22,66 @@ mongoose.connect(dbURI,{useUnifiedTopology:true}).then(
 app.listen(3000);
 
 // register view engine
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 // middleware & static files
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 
 // store a blog in database:
-app.get('/add-blog', (req,res) => {
-    const blog = new Blog({
-        title: 'Blogtitle2',
-        snippet: 'Blogsnippet2',
-        body: 'Blogbody2'
-    });
-    blog.save()
-    .then((result) => {
-        res.send(result)
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-});
+// app.get('/add-blog', (req,res) => {
+//     const blog = new Blog({
+//         title: 'Blogtitle2',
+//         snippet: 'Blogsnippet2',
+//         body: 'Blogbody2'
+//     });
+//     blog.save()
+//     .then((result) => {
+//         res.send(result)
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+// });
 
-app.get('/all-blogs',(req, res) => {
+// app.get('/all-blogs',(req, res) => {
     
-});
+// });
 
-app.get('/single-blog',(req, res) => {
-    Blog.findById('64a707751af67a1ff8ca3e7c')
-    .then((result) => {
-        res.send(result);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
+// app.get('/single-blog',(req, res) => {
+//     Blog.findById('64a707751af67a1ff8ca3e7c')
+//     .then((result) => {
+//         res.send(result);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// });
 
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  console.log('ip:', req.ip);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('new request made:');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   console.log('ip:', req.ip);
+//   next();
+// });
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  res.locals.path = req.path;
-  next();
-});
+// app.use((req, res, next) => {
+//   res.locals.path = req.path;
+//   next();
+// });
 
-// routes
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
-});
+// // routes
+// app.get('/', (req, res) => {
+//   res.redirect('/blogs');
+// });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
-});
+// app.get('/about', (req, res) => {
+//   res.render('about', { title: 'About' });
+// });
 
 
 // blog routes
@@ -92,23 +92,24 @@ app.get('/blogs', (req, res) => {
     .then((count) => {
         console.log( "Number of blogs:", count );
         blogsCount = count;
+        res.send(blogsCount);
     })
     .catch((err) => {
         console.log(err);
     })
 
-    Blog.find().sort({createdAt: -1})
-    .then((result) => {
-        res.render('index', { title: 'All Blogs', blogs: result, count: blogsCount });
-    })
-        .catch((err)=> {
-        console.log(err)
-    })
+    // Blog.find().sort({createdAt: -1})
+    // .then((result) => {
+    //     res.render('index', { title: 'All Blogs', blogs: result, count: blogsCount });
+    // })
+    //     .catch((err)=> {
+    //     console.log(err)
+    // })
       
 })
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
+// app.get('/blogs/create', (req, res) => {
+//   res.render('create', { title: 'Create a new blog' });
+// });
 
 
 
